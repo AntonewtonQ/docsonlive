2dddc12b5e0277a5733d23aa2132d5c9c2559b51
AntonewtonQ <antonewtonquima@gmail.com>
2024-09-25 22:27:46 +0100

    durooooooo

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -26,7 +26,7 @@ const CollaborativeRoom = ({
   const [loading, setLoading] = useState(false);
 
   const containerRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const updateTitleHandler = async (
     e: React.KeyboardEvent<HTMLInputElement>
@@ -91,7 +91,7 @@ const CollaborativeRoom = ({
                   placeholder="Enter Title"
                   onChange={(e) => setDocumentTitle(e.target.value)}
                   onKeyDown={updateTitleHandler}
-                  disable={!editing}
+                  disabled={!editing}
                   className="document-title-input"
                 />
               ) : (
