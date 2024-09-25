37891b9eb3175c4e206edd7344fd28a7b3f7eda0
AntonewtonQ <antonewtonquima@gmail.com>
2024-09-25 02:31:11 +0100

    acabou

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -6,6 +6,7 @@ import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
 import { redirect } from 'next/navigation';
+import { title } from 'process';
 
 export const createDocument = async ({userId, email}: CreateDocumentParams)=>{
     const roomId= nanoid();
@@ -89,7 +90,20 @@ export const updateDocumentAccess= async ({roomId, email, userType, updatedBy}: 
         })
 
         if(room){
-            // TODO: Send a notification to the user
+            const notificationId=nanoid();
+            await liveblocks.triggerInboxNotification({
+                userId:email,
+                kind:'$documentAccess',
+                subjectId:notificationId,
+                activityData:{
+                    userType,
+                    title:`You have been granted ${userType} access to he document by ${updatedBy.name}`,
+                    updatedBy:updatedBy.name,
+                    avatar:updatedBy.avatar,
+                    email:updatedBy.email
+                },
+                roomId
+            })
         }
 
         revalidatePath(`/documents/${roomId}`)
